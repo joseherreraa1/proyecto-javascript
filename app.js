ef6d6235ef209eb934e73f7c64532ce62f4fb6f7
Jose Herrera <joseherrera1655@gmail.com>
2022-08-24 19:33:21 -0300

    agrego ciclo for

---
+++ app.js
@@ -0,0 +1,8 @@
+
+    /* Algotitmo para una tabla de multiplicar */
+let numero = Number(prompt("Ingrese un numero para multiplicar"));
+
+for (let i = 1; i <= 10; i++) {
+  let resultado = numero * i;
+    alert(`${numero} X ${i} = ${resultado}`);
+}
